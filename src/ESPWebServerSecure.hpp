32cdeabe2219eb5e3e4805071e37aac133831aa9
Jack Jansen <Jack.Jansen@cwi.nl>
2020-02-08 00:27:50 +0100

    Added getServer()

--- src/ESPWebServerSecure.hpp
+++ src/ESPWebServerSecure.hpp
@@ -7,6 +7,7 @@
 
 class ESPWebServerSecure;
 
+// Placeholder class, to make the API conform to what Esp8266WebServerSecure provides.
 class ESPWebServerUnderlyingServer {
   friend class ESPWebServerSecure;
 protected:
@@ -26,6 +27,7 @@ public:
   ESPWebServerSecure(int port = 442);
   void setServerKeyAndCert(const uint8_t *key, int keyLen, const uint8_t *cert, int certLen);
   void setServerKeyAndCert_P(const uint8_t *key, int keyLen, const uint8_t *cert, int certLen);
+  ESPWebServerUnderlyingServer& getServer() { return _underlyingServer; }
 protected:
   ESPWebServerUnderlyingServer _underlyingServer;
   httpsserver::SSLCert _sslCert;
