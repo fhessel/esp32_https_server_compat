cdbb7188e66abe896ffc65faecbdfa6f7f3d4467
Jack Jansen <Jack.Jansen@cwi.nl>
2020-04-11 02:04:23 +0200

    Rationalized example to only show that three methods of form submission are identical

--- examples/HelloServer/HelloServer.ino
+++ examples/HelloServer/HelloServer.ino
@@ -35,74 +35,28 @@ void handleForm() {
   multi.toUpperCase();
   String rv;
   rv = "<html><head><title>Test Form</title></head><body>";
+  rv += "<p>Single line will be converted to lowercase, multi line to uppercase. You can submit the form with three different methods.</p>";
   rv += "<h2>Form using GET</h2>";
   rv += "<form method='GET'>";
   rv += "Single line:<br><input name='line' value='" + line + "'><br>";
   rv += "Multi line:<br><textarea name='multi' rows='8' cols='40'>" + multi + "</textarea><br>";
-  rv += "File:<br><input type='file' name='file'><br>";
   rv += "<input type='submit' value='upper+lower case'>";
   rv += "</form></body></html>";
   rv += "<h2>Form using POST urlencoded</h2>";
   rv += "<form method='POST' action='/form'>";
   rv += "Single line:<br><input name='line' value='" + line + "'><br>";
   rv += "Multi line:<br><textarea name='multi' rows='8' cols='40'>" + multi + "</textarea><br>";
-  rv += "File:<br><input type='file' name='file'><br>";
   rv += "<input type='submit' value='upper+lower case'>";
   rv += "</form></body></html>";
   rv += "<h2>Form using POST with multipart</h2>";
-  rv += "<form method='POST' action=\"/upload\" enctype=\"multipart/form-data\">";
+  rv += "<form method='POST' enctype=\"multipart/form-data\">";
   rv += "Single line:<br><input name='line' value='" + line + "'><br>";
   rv += "Multi line:<br><textarea name='multi' rows='8' cols='40'>" + multi + "</textarea><br>";
-  rv += "File:<br><input type='file' name='file'><br>";
   rv += "<input type='submit' value='upper+lower case'>";
   rv += "</form></body></html>";
   server.send(200, "text/html", rv);
 }
 
-void handleUpload() {
-  Serial.println("handleUpload() called");
-  String line = server.arg("line");
-  Serial.print("line: ");
-  Serial.println(line);
-  String multi = server.arg("multi");
-  Serial.print("multi: ");
-  Serial.println(multi);
-  server.send(200);
-}
-
-void handleUploadFile() {
-  Serial.println("handleUploadFile() called");
-  HTTPUpload& upload = server.upload();
-  if(upload.status == UPLOAD_FILE_START){
-    String filename = upload.filename;
-    Serial.printf("upload filename=%s\n", filename.c_str());
-#if 0
-    if(!filename.startsWith("/")) filename = "/"+filename;
-    Serial.print("handleFileUpload Name: "); Serial.println(filename);
-    fsUploadFile = SPIFFS.open(filename, "w");            // Open the file for writing in SPIFFS (create if it doesn't exist)
-    filename = String();
-#endif
-  } else if(upload.status == UPLOAD_FILE_WRITE){
-    Serial.printf("uploaded %d bytes\n", upload.currentSize);
-#if 0
-    if(fsUploadFile)
-      fsUploadFile.write(upload.buf, upload.currentSize); // Write the received bytes to the file
-#endif
-  } else if(upload.status == UPLOAD_FILE_END){
-    Serial.printf("upload total %d bytes\n", upload.totalSize);
-#if 0
-    if(fsUploadFile) {                                    // If the file was successfully created
-      fsUploadFile.close();                               // Close the file again
-      Serial.print("handleFileUpload Size: "); Serial.println(upload.totalSize);
-      server.sendHeader("Location","/success.html");      // Redirect the client to the success page
-      server.send(303);
-    } else {
-      server.send(500, "text/plain", "500: couldn't create file");
-    }
-#endif
-  server.send(200, "text/plain", "OK");
-  }
-}
 
 void handleNotFound() {
   digitalWrite(led, 1);
@@ -147,7 +101,6 @@ void setup(void) {
   server.on("/", handleRoot);
   server.on("/form", handleForm);
   server.on("/form", HTTP_POST, handleForm);
-  server.on("/upload", HTTP_POST, handleUpload, handleUploadFile);
   server.on("/inline", []() {
     server.send(200, "text/plain", "this works as well");
   });
